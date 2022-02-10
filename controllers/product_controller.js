4b76070fac71792b6b2dfdc3d1b9be1f770efbe2
0Apexx0 <adityaapexx@gmail.com>
2022-02-10 14:11:03 +0530

    adding actions to the controller folder in product_controller file

--- controllers/product_controller.js
+++ controllers/product_controller.js
@@ -1,11 +1,7 @@
 const Product = require('../model/product');
 
 
-
-
-
-module.exports.create = async (req,res)=>{
-    
+module.exports.createProduct = async (req,res)=>{
     
     try {
         const newProduct = new Product(req.body);
@@ -13,9 +9,56 @@ module.exports.create = async (req,res)=>{
         res.status(201).send(newProduct)
     } catch (err) {
         console.log(err);
-        res.status(400).send({message : "error"})
+        res.status(400).send({ message: "error" })
+    }
+}
+
+module.exports.fetchProducts = async (req,res)=>{
+
+    try {
+        const productData = await Product.find()
+        res.status(201).send(productData);
+
+    } catch (err) {
+        console.log(err);
+        res.status(400).send({ message: "error" })
     }
 
 
+} 
+module.exports.fetchOneProduct = async (req,res)=>{
 
-}
+    try {
+        const productData = await Product.findOne({ id: req.params.id })
+        console.log(productData);
+        res.status(201).send(productData);
+    } catch (err) {
+        console.log(err);
+        res.status(400).send({ message: "error" })
+    }
+
+} 
+module.exports.deleteProduct = async (req,res)=>{
+
+    try {
+        const productData = await Product.findOneAndDelete({ id: req.params.id }, req.body);
+
+        res.status(201).send(productData);
+    } catch (err) {
+        console.log(err);
+        res.status(400).send({ message: "error" })
+    }
+
+} 
+module.exports.updateProduct = async (req,res)=>{
+
+    try {
+        const productData = await Product.findOneAndUpdate({ id: req.params.id }, req.body);
+
+        res.status(201).send(productData);
+    } catch (err) {
+        console.log(err);
+        res.status(400).send({ message: "error" })
+    }
+
+} 
--- router/index.js
+++ router/index.js
@@ -2,78 +2,23 @@ const express = require('express');
 const Product = require('../model/product');
 const Counter = require('../model/id_counter');
 const router = express.Router();
-// const productController = require('../controllers/product_controller')
-
-router.post('/product', async (req, res) => {
+const productController = require('../controllers/product_controller')
 
 
-    try {
-        const newProduct = new Product(req.body);
-        await newProduct.save();
-        res.status(201).send(newProduct)
-    } catch (err) {
-        console.log(err);
-        res.status(400).send({ message: "error" })
-    }
+//create a new Product
+router.post('/product', productController.createProduct );
 
-
-
-});
-
-router.get('/product', async (req, res) => {
-    try {
-        const productData = await Product.find()
-        res.status(201).send(productData);
-
-    } catch (err) {
-        console.log(err);
-        res.status(400).send({ message: "error" })
-    }
-});
-
+//fetch all products
+router.get('/product', productController.fetchProducts) 
+    
 // fetch one product
-router.get('/product/:id', async (req, res) => {
-    console.log(req.params.id);
-    try {
-        const productData = await Product.findOne({ id: req.params.id })
-        console.log(productData);
-        res.status(201).send(productData);
-    } catch (err) {
-        console.log(err);
-        res.status(400).send({ message: "error" })
-    }
-
-});
-
+router.get('/product/:id', productController.fetchOneProduct) 
+   
 // fetch one product and update
-router.patch('/product/:id', async (req, res) => {
+router.patch('/product/:id',productController.updateProduct) 
 
-    try {
-        const productData = await Product.findOneAndUpdate({ id: req.params.id }, req.body);
-
-        res.status(201).send(productData);
-    } catch (err) {
-        console.log(err);
-        res.status(400).send({ message: "error" })
-    }
-
-
-})
-
-
-router.delete('/product/:id', async (req, res) => {
-
-    try {
-        const productData = await Product.findOneAndDelete({ id: req.params.id }, req.body);
-
-        res.status(201).send(productData);
-    } catch (err) {
-        console.log(err);
-        res.status(400).send({ message: "error" })
-    }
-    
-    
-})
+//delete a product
+router.delete('/product/:id',productController.deleteProduct ) 
 
 
 
